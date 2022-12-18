8c6a63dbaf8f525f715267f235f4f07f0cd874ab
GulsenZalova <zalova0202@gmail.com>
2022-12-19 01:02:21 +0400

    Country Quiz App Done

--- JS/app.js
+++ JS/app.js
@@ -1,78 +1,114 @@
 let capitals=[]
 let flags=[]
-let questionContainer=document.querySelector(".questionContainer")
+let quizQuestion=document.querySelector(".quiz-question")
+let quizAnswers=document.querySelector('.quiz-answers')
+let resultContainer=document.querySelector(".result-container")
+let quizContainer=document.querySelector(".quiz-container")
+let score=0
+let resultText=document.querySelector(".result-text")
+let againBtn=document.querySelector(".again-btn")
+function getAll(){
 fetch("https://restcountries.com/v3.1/all")
 .then(res=>res.json())
-.then(data=>{
+.then(data=>{chooseQuestions(data)
+})
+}
+getAll()
+function chooseQuestions(data){
     let randomCountryArr=[]
     data.forEach(element => {
         let randomCountryIndex=Math.floor(Math.random()*data.length)
         let randomCountry=data.slice(randomCountryIndex,randomCountryIndex+1)
-      if(randomCountryArr.length<5){
+      if(randomCountryArr.length<4){
         randomCountryArr.push(randomCountry)
       }
     });
-    chooseQuestions(randomCountryArr)
-    // console.log(randomCountryArr)
-    // console.log(capitals)
-    // console.log(flags)
-    let randomCapitalIndex=Math.floor(Math.random()*capitals.length)
-    let randomCapital=capitals.slice(randomCapitalIndex,randomCapitalIndex+1)
-    let randomFlagIndex=Math.floor(Math.random()*flags.length)
-    let randomFlag=flags.slice(randomFlagIndex,randomFlagIndex+1)
-    // console.log(randomCapitalIndex,randomCapital)
-    // console.log(randomFlagIndex,randomFlag)
-    let questions=[randomFlag,randomCapital]
-    let randomQuestionİndex=Math.floor(Math.random() * 2)
-
-    createQuestion(questions[randomQuestionİndex],randomQuestionİndex)
-
-    data.filter((element)=>{
-        if(element.capital==randomCapital[0]){
-            // console.log(element.name.common)
-        }
-    })
-    data.filter((element)=>{
-        if(element.flags.svg==randomFlag[0]){
-            // console.log(element.name.common)
-        }
-    })
-})
-
-
-
-
-
-function chooseQuestions(randomCountryArr){
     let randomCountryForQuestionIndex=Math.floor(Math.random()*randomCountryArr.length)
     let randomCountryForQuestion=randomCountryArr.slice(randomCountryForQuestionIndex,randomCountryForQuestionIndex+1)
     let randomCapital=randomCountryForQuestion[0][0].capital[0]
     let randomFlag=randomCountryForQuestion[0][0].flags.svg
     let questions=[randomFlag,randomCapital]
     let randomQuestionİndex=Math.floor(Math.random() * 2)
-    createQuestion(questions[randomQuestionİndex],randomQuestionİndex)
+    createQuestion(questions[randomQuestionİndex],randomQuestionİndex,randomCountryArr,randomCountryForQuestion)
 }
 
-
-
-function createQuestion(randomQuestion,randomQuestionİndex){
+function createQuestion(randomQuestion,randomQuestionİndex,randomCountryArr,randomCountryForQuestion){
+    // console.log(randomCountryForQuestion)
     let questionText
-    let capital=randomQuestion
    if(randomQuestionİndex==1){
-    console.log(capital)
-    questionText+=`
-    <div class="capitalQuestion">
-    <p>${randomQuestion} hansı ölkənin paytaxtıdır?</p>
-</div>
+    let capital=randomQuestion
+    questionText=`
+    <div class="quiz-question">
+    <span class="question">${capital}</span>
+    <span>is the capital of ?</span>
+    </div>
     `
-    questionContainer.innerHTML=questionText
+    quizQuestion.innerHTML=questionText
    }else if(randomQuestionİndex==0){
-    questionText+=`
-    <div class="flagQuestion">
-            <img src="${randomQuestion}" alt="">
-            <p> Şəkildəki hansı ölkənin bayrağıdır?</p>
+    let flag=randomQuestion
+    questionText=`
+        <div class="quiz-question">
+        <img src="${flag}" class="quiz-img" alt="">
+            <p> Witch country does this flag belong to?</p>
         </div>
     `
-    questionContainer.innerHTML=questionText
+    quizQuestion.innerHTML=questionText
    }
+
+   
+   randomCountryArr.forEach((element,i)=>{
+  let answers=`
+  <ul class="answers-list">
+  <li class="answer"><span class="variant">A</span><span class="variant-text">${randomCountryArr[0][0].name.common}</span></li>
+  <li class="answer"><span class="variant">B</span><span class="variant-text">${randomCountryArr[1][0].name.common}</span></li>
+  <li class="answer"><span class="variant">C</span><span class="variant-text">${randomCountryArr[2][0].name.common}</span></li>
+  <li class="answer"><span class="variant">D</span><span class="variant-text">${randomCountryArr[3][0].name.common}</span></li>
+</ul>
+  `
+
+   quizAnswers.innerHTML=answers 
+   let answersList=document.querySelectorAll(".answer")
+   let variantTexts=document.querySelectorAll(".variant-text")
+   let trueIcon=document.querySelector(".trueIcon")
+   let falseIcon=document.querySelector(".falseIcon")
+   let choosenVariant
+   answersList.forEach(answer=>{
+    answer.addEventListener("click",function(){
+        choosenVariant=answer.textContent.slice(1)
+        if(randomCountryForQuestion[0][0].name.common==choosenVariant){
+            // console.log("duzdu")
+            answer.classList.add("correct")
+            answer.innerHTML+=`<i class="fa-solid fa-check trueIcon"></i>`
+            score+=1
+            setTimeout(()=>{
+                getAll()
+            },1000)
+        }else{
+            // console.log("sehvdi")
+            answer.classList.add("uncorrect")
+            answer.innerHTML+=`<i class="fa-solid fa-x falseIcon"></i>`
+            variantTexts.forEach(x=>{
+                if(x.textContent==randomCountryForQuestion[0][0].name.common){
+                   x.parentElement.classList.add("correct")
+                   x.parentElement.innerHTML+=`<i class="fa-solid fa-check trueIcon"></i>`
+                }
+            
+            })
+            setTimeout(()=>{
+                resultContainer.classList.add("show")
+                quizContainer.classList.remove("show")
+                resultText.innerHTML=score
+            },1000)
+        }
+    })
+    })
+})
+
 }
+
+againBtn.addEventListener("click",function(){
+    score=0
+    resultContainer.classList.remove("show")
+    quizContainer.classList.add("show")
+    getAll()
+})
